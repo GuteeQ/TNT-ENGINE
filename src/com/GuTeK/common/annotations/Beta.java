f79cc66f32b36964616493a823c7573bfead3118
GuTeK <64864745+GuteeQ@users.noreply.github.com>
2022-01-03 20:03:48 +0100

    Nowości

---
+++ src/com/GuTeK/common/annotations/Beta.java
@@ -0,0 +1,14 @@
+/*
+    Autor: .GuTeK <developer@tntnetwork.pl>
+    Projekt: TNT-ENGINE
+    Zasoby: 1/2500
+    Data: 03.01.2022
+    Kontakt Discord: .GuTeK#0001
+    Kontakt e-mail: developer@tntnetwork.pl
+    Strony internetowe: tntnetwork.pl / market.tntnetwork.pl
+    ⓒ 2021 by .GuTeK | ALL RIGHTS RESERVED |
+*/
+
+package com.GuTeK.common.annotations;
+
+public @interface Beta {}
