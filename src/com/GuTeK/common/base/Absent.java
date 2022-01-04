117d8efc3b951f7a7f63adeff561d4e0d43dc163
GuTeK <64864745+GuteeQ@users.noreply.github.com>
2022-01-05 00:14:44 +0100

    Aktualizacja Absent.java (Aktualizacja kodu)

--- src/com/GuTeK/common/base/Absent.java
+++ src/com/GuTeK/common/base/Absent.java
@@ -6,7 +6,7 @@
     Kontakt Discord: .GuTeK#0001
     Kontakt e-mail: developer@tntnetwork.pl
     Strony internetowe: tntnetwork.pl / tnt-engine.ploo
-    ⓒ 2021 by .GuTeK | ALL RIGHTS RESERVED |
+    ⓒ 2022 by .GuTeK | ALL RIGHTS RESERVED |
 */
 
 package com.GuTeK.common.base;
@@ -25,5 +25,52 @@ final class Absent<T> extends Optional<T> {
 
     public boolean isPresent() {
         return false;
+    }
+
+    public T get() {
+        throw new IllegalStateException("Optional.get() cannot be called on an absent value");
+    }
+
+    public T or(T defaultValue) {
+        return Preconditions.checkNotNull(defaultValue, "use Optional.orNull() instead of Optional.or(null)");
+    }
+
+    public Optional<T> or(Optional<? extends T> secondChoice) {
+        return (Optional<T>)Preconditions.<Optional<? extends T>>checkNotNull(secondChoice);
+    }
+
+    public T or(Supplier<? extends T> supplier) {
+        return Preconditions.checkNotNull(supplier
+                .get(), "use Optional.orNull() instead of a Supplier that returns null");
+    }
+
+    @Nullable
+    public T orNull() {
+        return null;
+    }
+
+    public Set<T> asSet() {
+        return Collections.emptySet();
+    }
+
+    public <V> Optional<V> transform(Function<? super T, V> function) {
+        Preconditions.checkNotNull(function);
+        return Optional.absent();
+    }
+
+    public boolean equals(@Nullable Object object) {
+        return (object == this);
+    }
+
+    public int hashCode() {
+        return 2040732332;
+    }
+
+    public String toString() {
+        return "Optional.absent()";
+    }
+
+    private Object readResolve() {
+        return INSTANCE;
     }
 }
